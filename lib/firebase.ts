fc94e8fa4e9119c5092369517e26549fa180dd10
Abyesiza <79799754+Abyesiza@users.noreply.github.com>
2025-01-21 21:39:17 +0300

    api key invalid

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDoq2BDUDDcQyEaUwtipuoWKsYklIGQckY",
+  authDomain: "happy-safaris.firebaseapp.com",
+  projectId: "happy-safaris",
+  storageBucket: "happy-safaris.firebasestorage.app",
+  messagingSenderId: "600743540809",
+  appId: "1:600743540809:web:1879992dde1afe23af2798"
 };
 
 // Initialize Firebase
